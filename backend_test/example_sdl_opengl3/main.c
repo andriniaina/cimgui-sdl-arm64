ec3ce63fc5b2ffab6f41aa6f6deba71871e8e3e2
Andri <andriniaina@gmail.com>
2025-02-09 14:45:51 +0100

    remove gl

--- backend_test/example_sdl_opengl3/main.c
+++ backend_test/example_sdl_opengl3/main.c
@@ -7,8 +7,6 @@
 #ifdef _MSC_VER
 #include <windows.h>
 #endif
-#include <GL/gl.h>
-#include <GL/glu.h>
 
 #ifdef IMGUI_HAS_IMSTR
 #define igBegin igBegin_Str
@@ -28,46 +26,21 @@ int main(int argc, char* argv[])
     return -1;
   }
  
-  // Decide GL+GLSL versions
-#if __APPLE__
-    // GL 3.2 Core + GLSL 150
-    const char* glsl_version = "#version 150";
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-#else
-    // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-#endif
 
   // and prepare OpenGL stuff
   SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
-  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-  SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
   SDL_DisplayMode current;
   SDL_GetCurrentDisplayMode(0, &current);
   
   window = SDL_CreateWindow(
       "Hello", 0, 0, 1024, 768,
-      SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+      SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
       );
   if (window == NULL) {
     SDL_Log("Failed to create window: %s", SDL_GetError());
     return -1;
   }
 
-  SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-  SDL_GL_SetSwapInterval(1);  // enable vsync
-
-
-  // check opengl version sdl uses
-  SDL_Log("opengl version: %s", (char*)glGetString(GL_VERSION));
 
   // setup imgui
   igCreateContext(NULL);
@@ -81,8 +54,6 @@ int main(int argc, char* argv[])
   ioptr->ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
 #endif
   
-  ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-  ImGui_ImplOpenGL3_Init(glsl_version);
 
   igStyleColorsDark(NULL);
   //ImFontAtlas_AddFontDefault(io.Fonts, NULL);
@@ -113,7 +84,6 @@ int main(int argc, char* argv[])
     }
     
     // start imgui frame
-    ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplSDL2_NewFrame();
     igNewFrame();
     ImGui_ImplSDLRenderer2_Init(NULL);
@@ -161,30 +131,19 @@ int main(int argc, char* argv[])
 
     // render
     igRender();
-    SDL_GL_MakeCurrent(window, gl_context);
-    glViewport(0, 0, (int)ioptr->DisplaySize.x, (int)ioptr->DisplaySize.y);
-    glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
-    glClear(GL_COLOR_BUFFER_BIT);
-    ImGui_ImplOpenGL3_RenderDrawData(igGetDrawData());
 #ifdef IMGUI_HAS_DOCK
 	if (ioptr->ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
         {
-            SDL_Window* backup_current_window = SDL_GL_GetCurrentWindow();
-            SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
             igUpdatePlatformWindows();
             igRenderPlatformWindowsDefault(NULL,NULL);
-            SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
         }
 #endif
-    SDL_GL_SwapWindow(window);
   }
 
   // clean up
-  ImGui_ImplOpenGL3_Shutdown();
   ImGui_ImplSDL2_Shutdown();
   igDestroyContext(NULL);
 
-  SDL_GL_DeleteContext(gl_context);
   if (window != NULL)
   {
     SDL_DestroyWindow(window);
